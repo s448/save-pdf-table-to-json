b02d139189b1dfd1750e5418fd93ee0adc4603f2
s448 <saeed01125940652@gmail.com>
2025-03-15 01:59:54 +0200

    adding the file data.pdf

--- tablua.py
+++ tablua.py
@@ -1,6 +1,6 @@
 import tabula
 
-pdf_path = "https://github.com/chezou/tabula-py/raw/master/tests/resources/data.pdf"
+pdf_path = "https://github.com/s448/save-pdf-table-to-json/blob/main/examle.pdf"
 
 dfs = tabula.read_pdf(pdf_path,pages=1, stream=True,output_format="json")
 # read_pdf returns list of DataFrames
